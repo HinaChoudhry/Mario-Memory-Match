eceb8df03b173b7b90240a0f91267233c3504064
HinaChoudhry <hinachoudhry@msn.com>
2021-04-16 15:24:33 +0000

    Added new classes to the playing cards, assigning them to easy, hard or medium and made JS functions so that depending on which difficulty is selected a different number of cards generate on the playing field.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,36 @@
 // Difficulty modal and close button 
 
-document.getElementById("button").addEventListener("click",
-    function () {
-        document.querySelector(".bg-modal").style.display = "flex";
-    });
-
-document.querySelector(".close-button").addEventListener("click", function () {
+document.querySelector(".close-button").addEventListener("click", function() {
     document.querySelector(".bg-modal").style.display = "none";
 });
 
 
-// Generates number of cards based on difficulty selected
 
+// Generates number of cards based on difficulty selected
+$("#selectEasy").click(function(){
+    easyButton();
+});
+
+function easyButton() {
+$(".medium, .hard").addClass("invisible").removeClass("visible");
+};
+
+
+
+$("#selectMedium").click(function() {
+    mediumButton();
+});
+
+function mediumButton(){
+    $('.medium').addClass('visible').removeClass('invisible');
+    $(".hard").addClass('invisible').removeClass('visible');
+};
+
+$("#selectHard").click(function(){
+    hardButton();
+});
+
+function hardButton(){
+    $(".medium").addClass("visible").removeClass("invisible");
+    $(".hard").addClass("visible").removeClass("invisible");
+};
