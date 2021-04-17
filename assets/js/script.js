2817e62c5934098f13d06a7001958bfd5eb99105
HinaChoudhry <hinachoudhry@msn.com>
2021-04-17 09:36:25 +0000

    The way I did the Javascript for a different number of cards to show per game level selected is not worknig with my CSS. THe addClass(visible) and removeClass(invisible) are interfering with my .card.visible class in CSS so the cards are already flipped over when the page loads. Redoing the JS for the difficulty selection.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,30 +7,4 @@ document.querySelector(".close-button").addEventListener("click", function() {
 
 
 // Generates number of cards based on difficulty selected
-$("#selectEasy").click(function(){
-    easyButton();
-});
 
-function easyButton() {
-$(".medium, .hard").addClass("invisible").removeClass("visible");
-};
-
-
-
-$("#selectMedium").click(function() {
-    mediumButton();
-});
-
-function mediumButton(){
-    $('.medium').addClass('visible').removeClass('invisible');
-    $(".hard").addClass('invisible').removeClass('visible');
-};
-
-$("#selectHard").click(function(){
-    hardButton();
-});
-
-function hardButton(){
-    $(".medium").addClass("visible").removeClass("invisible");
-    $(".hard").addClass("visible").removeClass("invisible");
-};
