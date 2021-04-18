61fbe62e424de7890e7340665ae6b63485ab8498
HinaChoudhry <hinachoudhry@msn.com>
2021-04-18 13:18:20 +0000

    Created the Javascript code so that when a user selects a difficulty, it gives output which is currently only an alert. The JS to generate the number of cards needs to be added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,3 +8,16 @@ document.querySelector(".close-button").addEventListener("click", function() {
 
 // Generates number of cards based on difficulty selected
 
+let easy = document.getElementById("selectEasy");
+let medium = document.getElementById("selectMedium");
+let hard = document.getElementById("selectHard");
+
+easy.addEventListener('click', () => game(5));
+medium.addEventListener('click', () => game(7));
+hard.addEventListener('click', () => game(9));
+
+function game(num) {
+  if (num === 5) {
+    alert("Success!")
+  }
+  };
